b446e37a9d579f2e11e8ad4b35aee9b25867fda4
Ejidike <ejidikestanley64@outlook.com>
2021-10-10 11:24:53 -0500

    Created new project

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace IIS_mvc.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
